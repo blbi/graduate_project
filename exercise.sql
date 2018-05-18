3d24236da4c4c82e0c8f8481195c137b3f9ab81d
thakd1011 <thakd1011@naver.com>
2018-05-18 20:34:59 +0900

    modified spell

--- exercise.sql
+++ exercise.sql
@@ -7,7 +7,7 @@ create table ROUTINE (
 );
 
 create table EXERCISE (
-	sec varchar(1) not null,
+	sex varchar(1) not null,
 	length varchar(3) not null,
 	bodypart varchar(8) not null,
 	excer_type varchar(10) not null,
