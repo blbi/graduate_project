aacbf5a9a2475e6bccb7e5b7814901e295419923
thakd1011 <thakd1011@naver.com>
2018-08-25 15:08:43 +0900

    modified table and add test_data.sql file

--- exercise.sql
+++ exercise.sql
@@ -1,30 +1,32 @@
 create table ROUTINE (
 	video_num int(7) not null,
-	sex varchar(1) not null,
+	sex varchar(2) not null,
+	length int(3) not null,
 	timing varchar(10) not null,
-	bodypart varchar(8) not null,
-	url varchar(15) not null,
+	bodypart varchar(20) not null,
+	level varchar(2) not null,
+	url varchar(100) not null,
 	primary key (video_num)
 );
 
 create table EXERCISE (
 	video_num int(7) not null,
-	sex varchar(1) not null,
-	length varchar(3) not null,
-	bodypart varchar(8) not null,
+	sex varchar(2) not null,
+	length int(3) not null,
+	timing varchar(10) not null,
+	bodypart varchar(20) not null,
 	excer_type varchar(10) not null,
 	trainer varchar(15) not null,
 	equipment varchar(20) not null,
-	timing varchar(10) not null,
-	level varchar(1) not null,
-	url varchar(15) not null,
+	level varchar(2) not null,
+	url varchar(100) not null,
 	primary key (video_num)
 );
 
 create table USER (
 	user_num int(6) not null,
 	sex varchar(1) not null,
-	bodypart varchar(8) not null,
+	bodypart varchar(20) not null,
 	muscle varchar(1) not null,
 	endurance varchar(1) not null,
 	age int(3) not null,
@@ -37,6 +39,5 @@ create table HISTORY (
 	user_num int(7) not null,
 	video_num int(7) not null,
 	rating int(1) not null,
-	time timestamp default current_timestamp on update current_timestamp,
-	primary key(user_num)
+	time timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
